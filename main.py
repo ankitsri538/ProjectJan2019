f3572373af10494bfed064abedbe2b75abbfc461
ankitsri538 <amby2020@gmail.com>
2019-01-13 14:24:06 +0530

    Pirple homework 1

---
+++ main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 13 12:49:57 2019
+
+@author: ankit
+"""
+
+
+Song = ": Pal"
+Artist = ": Javed - Mohsin, Arijit Singh, Shreya Ghoshal"
+Music = ": Javed - Mohsin"
+Lyrics = ": Prashant Ingole & Kunaal Vermaa"
+Song_Programmed_Sound_Design = ": Dj Phukan"
+Asst_programmer = ": Madhab Deka"
+Live_Guitars  = ": Ankur Mukherjee"
+Live_Flute  = ": P.M.K Naveen Kumar"
+Shreya_Ghoshal_Vocals_Recorded_by = ": Amey Londhe At Audio Garage Studio."
+Live_Instruments_recorded  =": Pankaj Borah at Neo Sound studio" 
+Assistant_Mix_engineer = ": Michael Edwin Pillai"
+
+print ("{}".format(Song),
+"{}".format(Artist),
+"{}".format(Music),
+"{}".format(Lyrics),
+"{}".format(Song_Programmed_Sound_Design),
+"{}".format(Asst_programmer),
+"{}".format(Live_Guitars),
+"{}".format(Live_Flute),
+"{}".format(Shreya_Ghoshal_Vocals_Recorded_by),
+"{}".format(Live_Instruments_recorded),
+"{}".format(Assistant_Mix_engineer),
+sep="\n")
