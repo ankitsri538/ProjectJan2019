3466f6cda47c786a62b65b8db0abd8989c270da7
ankitsri538 <amby2020@gmail.com>
2019-01-13 20:50:36 +0530

    Pirple homework 2

---
+++ Assignment2.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 13 20:44:45 2019
+
+@author: ankit
+"""
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 13 12:49:57 2019
+
+@author: ankit
+"""
+
+#Detail about new song PAL available on YouTube
+
+Songs = ": Pal"
+Artists = ": Javed - Mohsin, Arijit Singh, Shreya Ghoshal"
+Years = "2019"
+Genr = "Indian"
+Music = ": Javed - Mohsin"
+Lyrics = ": Prashant Ingole & Kunaal Vermaa"
+Song_Programmed_Sound_Design = ": Dj Phukan"
+Asst_programmer = ": Madhab Deka"
+Live_Guitars  = ": Ankur Mukherjee"
+Live_Flute  = ": P.M.K Naveen Kumar"
+Shreya_Ghoshal_Vocals_Recorded_by = ": Amey Londhe At Audio Garage Studio."
+Live_Instruments_recorded  =": Pankaj Borah at Neo Sound studio" 
+Assistant_Mix_engineer = ": Michael Edwin Pillai"
+
+
+def Song(attribute):
+    print(attribute)
+    return
+
+def Genre(attribute):
+    print(attribute)
+    return
+
+def Year(attribute):
+    print(attribute)
+    return    
+
+Song(Songs)
+Year(Years)
+Genre(Genr)
+
+    
