8407d6e4b96b06e1f7552aab2b5817cc451feb0c
ankitsri538 <amby2020@gmail.com>
2019-01-25 09:13:04 +0530

    Pirpe Assignment3

---
+++ Assignment3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 25 09:01:13 2019
+
+@author: ankit
+"""
+
+def ncomparator(n1,n2,n3):
+    if int(n1)==int(n2) or int(n1) == int(n3) or int(n2) == int(n3):
+        print("True")
+    else:
+        print("False")
+        
+        
+        
+ncomparator('6',2,6)        
+        
